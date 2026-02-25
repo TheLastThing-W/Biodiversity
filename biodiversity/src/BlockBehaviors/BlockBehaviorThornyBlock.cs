3731c7a0660552f86637aa243b93087a0b08766e
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2026-02-25 13:56:44 -0600

    add thorny block behavior

---
+++ biodiversity/src/BlockBehaviors/BlockBehaviorThornyBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.API.Common;
+
+namespace biodiversity.src.BlockBehaviors
+{
+    internal class BlockBehaviorThornyBlock : BlockBehavior
+    {
+        public BlockBehaviorThornyBlock(Block block) : base(block)
+        {
+            this.block = block;
+        }
+    }
+}
